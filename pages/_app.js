04a60a450a25a4c611d1f45873ac3e46125e7fdc
Denis Shutock <yoshutock@gmail.com>
2022-09-12 22:34:39 +0300

    Promise it will fix deploying error

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,11 @@
 import Head from 'next/head'
 import '../styles/global.scss'
-import favicon from '../public/favicon.ico'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel="icon" href={favicon} type="image/icon" />
+        <link rel="icon" href='./public/favicon.ico' type="image/icon" />
       </Head>
       <Component {...pageProps} />
     </>
