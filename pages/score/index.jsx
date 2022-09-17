fb16f99464c0ec5d26ac903bc038a6bdd2c2fd36
Denis Shutock <yoshutock@gmail.com>
2022-09-17 09:04:53 +0300

    /score improvement

--- pages/score/index.jsx
+++ pages/score/index.jsx
@@ -1,79 +1,23 @@
-import dynamic from "next/dynamic";
-
-import MainLayout from "../../layouts/MainLayout";
-
-import { Score, Pulse } from "../../components/common/Card";
-import Wallet from "../../components/common/Wallet";
-
-const Achievement = dynamic(() => import("../../components/common/Card"), {
-  ssr: false,
-});
+import Link from "next/link";
+import MainLayout from "../../layouts/mainLayout";
 
 export default function GetScrore() {
-  const wallet = {
-    address: "artyshatilov.eth",
-    blockchain: "eth",
-    userpick: "/userpick.png",
-    score: 69.69,
-    age: { months: 46 },
-    balance: 0.07,
-    turnover: 44.94,
-    spendings: 32,
-    activity: [0, 2, 1, 3, 6, 2, 4, 1, 6, 8, 4, 9],
-    recentActivity: ["Uniswap", "Rainbow", "OpenSea", "AAVE"],
-  };
-  wallet.age.years = Math.round(wallet.age.months / 12);
-  const spendings = Math.round(wallet.spendings / 10) * 10;
   return (
     <MainLayout title="Get Score">
       <div className="wrapper">
-        <section className="code">
-          <pre>{`wallet = {
-  address: "${wallet.address}",
-  blockchain: "${wallet.blockchain}",
-  userpick: { userpick },
-  score: ${wallet.score},
-  age: { months: ${wallet.age.months} },
-  balance: ${wallet.balance},
-  turnover: ${wallet.turnover},
-  spendings: ${wallet.spendings},
-  activity: [0, 2, 1, 3, 6, 2, 4, 1, 6, 8, 4, 9],
-  recentActivity: ["Uniswap", "Rainbow", "OpenSea", "AAVE"],
-};
-  `}</pre>
-        </section>
-
-        <section className="getScore">
-          <div className="container information">
-            <section className="highlights">
-              <div className="container">
-                <h4>Highlights</h4>
-              </div>
-              <div className="container cards">
-                <Score score={wallet.score} />
-                <Pulse />
-                <Achievement
-                  emoji="old"
-                  title="The Ancesor"
-                  description="You have a pretty old wallet—more than 4 years since the first transaction."
-                />
-                <Achievement
-                  emoji="old"
-                  title="Big Spender"
-                  description={`This wallet has a total spendings of more than Ξ${spendings}.`}
-                />
-              </div>
-            </section>
+        <section>
+          <h1 className="container">Future Get Score Page</h1>
+          <div className="container column start paragraph">
+            <p>It should be the input wallet page.</p>
+            <p>Try out these two prototypes:</p>
           </div>
-
-          <div className="container wallet">
-            <Wallet
-              userpick={wallet.userpick}
-              address={wallet.address}
-              balance={wallet.balance}
-              turnover={wallet.turnover}
-              age={wallet.age.years}
-            />
+          <div className="container start">
+            <Link href="/score/artyshatilov.eth">
+              <a className="underline">artyshatilov.eth</a>
+            </Link>
+            <Link href="/score/shutock.eth">
+              <a className="underline">shutock.eth</a>
+            </Link>
           </div>
         </section>
       </div>
