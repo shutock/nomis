3a89059f6f31d6e8217ff19a5ef6b403ff3c3c97
Denis Shutock <yoshutock@gmail.com>
2022-10-03 14:40:34 +0600

    Added placeholders for blockchains

--- maps/blockchains.js
+++ maps/blockchains.js
@@ -3,35 +3,42 @@ export const blockchains = [
     item: "Aeternity",
     slug: "aeternity",
     icon: "/blockchains/aeternity.svg",
+    placeholder: "ak_... or .chain",
   },
   {
     item: "Binance",
     slug: "bsc",
     icon: "/blockchains/binance.svg",
+    placeholder: "0x... or .bnb",
   },
   {
     item: "Ethereum",
     slug: "ethereum",
     icon: "/blockchains/ethereum.svg",
+    placeholder: "0x... or .eth",
   },
   {
     item: "Evmos",
     slug: "evmos",
     icon: "/blockchains/evmos.svg",
+    placeholder: "0x or .evns",
   },
   {
     item: "Polygon",
     slug: "polygon",
     icon: "/blockchains/polygon.svg",
+    placeholder: "0x.. or .poly",
   },
   {
     item: "Ripple",
     slug: "ripple",
     icon: "/blockchains/ripple.svg",
+    placeholder: "r... address",
   },
   {
     item: "Solana",
     slug: "solana",
     icon: "/blockchains/solana.svg",
+    placeholder: "Solana address or .sol",
   },
 ];
