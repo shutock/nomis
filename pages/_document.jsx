9ed7ab84beeeb0343339a023ca858072161e8efd
Denis Shutock <yoshutock@gmail.com>
2022-10-10 15:24:08 +0600

    Bug fixes

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -10,7 +10,6 @@ export default function Document() {
         <meta name="author" content="Nomis" />
         <meta name="description" content={description} />
         <meta name="og:description" content={description} />
-        <link rel="shortcut icon" href="nomisIcon.svg" type="svg/xml" />
       </Head>
       <body className="dark">
         <Main />
